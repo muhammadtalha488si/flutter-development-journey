2460620e27a85802644f63a7b97f4e23f323de78
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-07-03 23:02:55 +0500

    add new cood

---
+++ Dart language/opp concepts.dart/main.dart
@@ -0,0 +1,18 @@
+//Encapsulation concept
+//how we sue getter and setter
+/*
+import 'person.dart';
+
+void main(){
+
+Person person=Person();
+
+person.setName ="ali";
+person.setAge=21;
+person.setCountry="pakistan";
+print(person.getName);
+print(person.getAge);
+print(person.getCountry);
+}
+
+       */
---
+++ Dart language/opp concepts.dart/person.dart
@@ -0,0 +1,18 @@
+//Encapsulation concept
+//how we sue getter and setter
+/*
+class Person {
+ late String _name;
+ late int _age;
+ late String _country;
+
+  set setName(String name) => _name=name;
+  set setAge(int age )=>_age=age;
+   set setCountry(String country)=>_country=country;
+
+ String get getName => _name;
+ int get getAge=> _age;
+ String get getCountry=>_country;
+}
+       */
+
