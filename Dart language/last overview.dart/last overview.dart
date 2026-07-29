c71519800cd7a677192977dce11c70173b3f646e
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-07-29 12:19:41 +0500

    add new code

---
+++ Dart language/last overview.dart/last overview.dart
@@ -0,0 +1,20 @@
+// Map concept__________________________
+
+// void main(){
+
+//   Map<String,dynamic>map={
+//     'name':'muhammad talha',
+//     'age':21
+//   };
+//   print(map['name']);
+// }
+
+// constant value concept____________________
+
+void main(){
+print(Gender.Male.name);
+}
+
+enum Gender{Male,Female,other}
+
+enum Days{moday,tuesday,wensday}
