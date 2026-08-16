60431bf992264039f76d67c5c9bfb4e928ea559f
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-08-16 16:17:12 +0500

    add new file

--- flutter_first_project/lib/container widgets/main file/main.dart
+++ flutter_first_project/lib/container widgets/main file/main.dart
@@ -1,16 +1,16 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_first_project/container%20widgets/main%20file/screens/home_screens.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_first_project/container%20widgets/main%20file/screens/home_screens.dart';
 
-void main(){
-runApp(MyApp());
-}
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// void main(){
+// runApp(MyApp());
+// }
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreens(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: HomeScreens(),
+//     );
+//   }
+// }
