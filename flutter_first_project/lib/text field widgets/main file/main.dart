5c10eb22eb4d4331723643365fdc521d7f664389
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-08-17 22:19:03 +0500

    add new file

---
+++ flutter_first_project/lib/text field widgets/main file/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_first_project/text%20field%20widgets/screens/home_screens.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreens(),
+    );
+  }
+}
