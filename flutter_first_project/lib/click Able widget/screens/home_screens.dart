87b03aff72cf38810a1ad0ec796a52fa22fb524f
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-08-18 03:17:47 +0500

    add new file

---
+++ flutter_first_project/lib/click Able widget/screens/home_screens.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+class HomeScreens extends StatelessWidget {
+  const HomeScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text('HOME'),
+      ),
+      body: Column(
+        children: [
+           InkWell(
+            onTap: (){
+              print('clicked=================================>');
+            },
+            onDoubleTap: () {
+              print("on pressed click======================>");
+            },
+            child: Text(
+              'HELO WORLD',
+              style: TextStyle(fontSize:30),
+              ),
+          )
+          // GestureDetector(
+          //   onTap: (){
+          //     print('clicked=================================>');
+          //   },
+          //   onDoubleTap: () {
+          //     print("on pressed click======================>");
+          //   },
+          //   child: Text(
+          //     'HELO WORLD',
+          //     style: TextStyle(fontSize:30),
+          //     ),
+          // )
+        ],
+      ),
+    );
+  }
+}
