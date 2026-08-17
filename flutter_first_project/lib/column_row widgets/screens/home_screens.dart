2b16e122a9ef9057a72d6888b3faf6c744ffbc58
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-08-17 14:21:00 +0500

    add new file

---
+++ flutter_first_project/lib/column_row widgets/screens/home_screens.dart
@@ -0,0 +1,63 @@
+// import 'package:flutter/material.dart';
+//  class HomeScreens extends StatelessWidget {
+//   const HomeScreens({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.blue,
+//         title: Text(
+//           'Home',
+//           style: TextStyle(
+//             color:const Color.fromARGB(255, 88, 25, 2), 
+//             fontSize: 30,
+//           ),
+//           ),
+//       ),
+//       body: Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceAround,
+//         crossAxisAlignment:CrossAxisAlignment.stretch,
+//         children: [
+//          Container(
+//             height: 100,
+//             width: 100,
+//             color: const Color.fromARGB(255, 51, 40, 1),
+//           ),
+//            Container(
+//             height: 100,
+//             width: 100,
+//             color: const Color.fromARGB(255, 93, 64, 255),
+//           ),
+//           Container(
+//             height: 100,
+//             width: 100,
+//             color: const Color.fromARGB(255, 255, 64, 77),
+//           )
+//         ],
+//       ),
+//       // body: Column(
+//       //   spacing: 40,
+//       //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//       //   crossAxisAlignment: CrossAxisAlignment.stretch,
+//       //   children: [
+//       //    Container(
+//       //       height: 150,
+//       //       width: 150,
+//       //       color: const Color.fromARGB(255, 51, 40, 1),
+//       //     ),
+//       //      Container(
+//       //       height: 150,
+//       //       width: 150,
+//       //       color: const Color.fromARGB(255, 93, 64, 255),
+//       //     ),
+//       //     Container(
+//       //       height: 150,
+//       //       width: 150,
+//       //       color: const Color.fromARGB(255, 255, 64, 77),
+//       //     )
+//       //   ],
+//       // ),
+//     );
+//   }
+// }
