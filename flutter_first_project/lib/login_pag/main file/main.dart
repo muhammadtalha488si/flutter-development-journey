70bad80504a35ee3ca169e7ad2960c3a50631b78
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-08-19 22:43:55 +0500

    add new file

--- flutter_first_project/lib/login_pag/main file/main.dart
+++ flutter_first_project/lib/login_pag/main file/main.dart
@@ -1,18 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_first_project/login_pag/screens/login_screens.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_first_project/login_pag/screens/login_screens.dart';
 
-void main(){
-    runApp(MyApp());
-}
+// void main(){
+//     runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home:Homescreen() ,
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home:Homescreen() ,
+//     );
+//   }
+// }
--- flutter_first_project/lib/login_pag/screens/login_screens.dart
+++ flutter_first_project/lib/login_pag/screens/login_screens.dart
@@ -1,75 +1,130 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class Homescreen extends StatefulWidget {
-  const Homescreen({super.key});
+// class Homescreen extends StatefulWidget {
+//   const Homescreen({super.key});
 
-  @override
-  State<Homescreen> createState() => _HomescreenState();
-}
+//   @override
+//   State<Homescreen> createState() => _HomescreenState();
+// }
 
-class _HomescreenState extends State<Homescreen> {
-  TextEditingController email=TextEditingController();
-  TextEditingController password=TextEditingController();
-  GlobalKey formKey =GlobalKey<FormState>();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar( 
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Form(
-          key: formKey,
-          child: Column(
-            spacing: 15,
-            children: [
-              FlutterLogo(size: 100),
-            Text('Login Hare',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
-            Text('plese enter your email & password to get started with your accout',style: TextStyle(fontWeight:FontWeight.w600),),
-              TextField(
-                controller: email,
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                 border: OutlineInputBorder()
-                ),
-              ),
-          
-               TextField(
-                controller: password,
-                decoration: InputDecoration(
-                  hintText: 'password',
-                 border: OutlineInputBorder()
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                height: 55,
-                child: ElevatedButton(
-                  onPressed: (){},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.white,
-                  ),
-                  child: Text('login'),
-                ),
-              ),
-              TextButton(
-                onPressed: (){},
-                child: Text('forgot password ?'),
-              ),
-              Spacer(),
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: OutlinedButton(
-                  onPressed: (){},
-                  child: Text('Do not have acount ? Register Hare'),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+// class _HomescreenState extends State<Homescreen> {
+//   TextEditingController email=TextEditingController();
+//   TextEditingController password=TextEditingController();
+//   GlobalKey<FormState> formKey =GlobalKey<FormState>();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Form(
+//         key: formKey, 
+//         child: Stack(
+//           children: [
+//             ListView(
+//           padding: EdgeInsets.all(20),
+//           children: [
+//             SizedBox(height: 100),
+//             FlutterLogo(size: 100),
+//             SizedBox(height: 15),
+
+//           Text(
+//             'Login Hare',
+//             textAlign: TextAlign.center,
+//               style: TextStyle(
+//                 fontSize: 25,
+//                 fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+
+//           Text(
+//             'plese enter your email & password to get started with your accout',
+//             style: TextStyle(
+//               fontWeight:FontWeight.w600
+//               ),
+//               ),
+            
+//             SizedBox(height: 15,),
+//             TextFormField(
+//               controller: email,
+//               decoration: InputDecoration(
+//                 hintText: 'Email',
+//                border: OutlineInputBorder()
+//               ),
+//              validator: (value){
+//               if(value ==null||value.isEmpty){
+//                 return 'plese enter your email';
+//               }
+//               return null;
+//              },
+//             ),
+        
+//             SizedBox(height: 15,),      
+//              TextFormField(
+//               controller: password,
+//               decoration: InputDecoration(
+//                 hintText: 'password',
+//                border: OutlineInputBorder()
+//               ),
+//                validator: (value){
+//               if(value ==null||value.isEmpty){
+//                 return 'plese enter your password';
+//               }
+//               return null;
+//              },
+//             ),
+
+//             SizedBox(height: 15,),
+//             SizedBox(
+//               width: double.infinity,
+//               height: 55,
+//               child: ElevatedButton(
+//                 onPressed: (){
+//                   if(formKey.currentState!.validate());
+//                 },
+//                 style: ElevatedButton.styleFrom(
+//                   backgroundColor: Colors.blue,
+//                   foregroundColor: Colors.white,
+//                 ),
+//                 child: Text('login'),
+//               ),
+//             ),
+
+//             SizedBox(height: 10,),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.end,
+//               children: [
+//                 TextButton(
+//                   onPressed: (){},
+//                   child: Text('forgot password ?'),
+//                 ),
+//               ],
+//             ),
+
+//             SizedBox(height: 10,),
+//             SizedBox(
+//               width: double.infinity,
+//               height: 50,
+//               child: OutlinedButton(
+//                 onPressed: (){},
+//                 child: Text('Do not have acount ? Register Hare'),
+//               ),
+//             )
+//           ],
+//         ),
+//         Positioned(
+//           top: -130,
+//           right: -130,
+//           child: Container(         
+//             height: 300,
+//             width: 300,
+//             decoration: BoxDecoration(
+//               color: Colors.blue,
+//               shape: BoxShape.circle,
+//             ),
+//           ),
+//         )
+//           ],
+//         )
+//       ),
+//     );
+//   }
+// }
