cde58b6a7a0646a646e55f4367efb32626bd47d5
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-08-21 02:47:21 +0500

    add new file

--- flutter_first_project/lib/navigation_drawer/screens/home_screens.dart
+++ flutter_first_project/lib/navigation_drawer/screens/home_screens.dart
@@ -12,10 +12,63 @@ class _HomeScreensState extends State<HomeScreens> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.pinkAccent,
-        title: Text('HOME PAGE',textAlign: TextAlign.center,),
+        backgroundColor: const Color.fromARGB(255, 64, 201, 255),
+        title: Text('HOME PAGE',),
       ),
-      drawer: Drawer(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.all(30),
+          children: [
+            SizedBox(height: 50,),
+          FlutterLogo(size: 120,),
+          SizedBox(height: 20,),
+          Text('App Title',textAlign: TextAlign.center,style: TextStyle(fontSize: 30),),
+          SizedBox(height: 20,),
+
+           ListTile(
+            onTap: (){
+              print('clicked');
+            },
+            title: Text("Home"),
+            leading: Icon(Icons.home),
+           ),
+
+            ListTile(
+            onTap: (){
+              print('clicked');
+            },
+            title: Text("Setting"),
+            leading: Icon(Icons.settings),
+           ),
+
+            ListTile(
+            onTap: (){
+              print('clicked');
+            },
+            title: Text("About"),
+            leading: Icon(Icons.info),
+           ),
+
+            ListTile(
+            onTap: (){
+              print('clicked');
+            },
+            title: Text("Helf"),
+            leading: Icon(Icons.help),
+           ),
+
+            ListTile(
+            onTap: (){
+              print('clicked');
+            },
+            title: Text("Logout"),
+            leading: Icon(Icons.logout),
+           ),
+
+
+          ],
+        ),
+      ),
     );
   }
 }
