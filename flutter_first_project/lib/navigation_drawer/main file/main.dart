c011a814e0be5a9c1db033654c0f09360d014ef5
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-08-20 16:15:20 +0500

    add new file

---
+++ flutter_first_project/lib/navigation_drawer/main file/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_first_project/navigation_drawer/screens/home_screens.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreens(),
+    );
+  }
+}
