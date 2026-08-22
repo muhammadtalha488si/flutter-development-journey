9552af1c9c1537fe18490fc6f2ec99ab25f6b660
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-08-22 14:47:58 +0500

    add new file

--- flutter_first_project/lib/different widgets/main file/main.dart
+++ flutter_first_project/lib/different widgets/main file/main.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_first_project/different%20widgets/screens/home_screens.dart';
-
-void main(){
-  runApp(MyApp());
-}
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreens(),
-    );
-  }
-}
+// 
--- flutter_first_project/lib/different widgets/screens/home_screens.dart
+++ flutter_first_project/lib/different widgets/screens/home_screens.dart
@@ -1,230 +1,230 @@
-// ignore_for_file: deprecated_member_use
+// // ignore_for_file: deprecated_member_use
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
 
-class HomeScreens extends StatefulWidget {
-  const HomeScreens({super.key});
+// class HomeScreens extends StatefulWidget {
+//   const HomeScreens({super.key});
 
-  @override
-  State<HomeScreens> createState() => _HomeScreensState();
-}
+//   @override
+//   State<HomeScreens> createState() => _HomeScreensState();
+// }
 
-class _HomeScreensState extends State<HomeScreens> {
-  bool isConnected=false;
-  String? groupValue='one';
+// class _HomeScreensState extends State<HomeScreens> {
+//   bool isConnected=false;
+//   String? groupValue='one';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 88, 72, 72),
-        title: Text('Home',style: TextStyle(color: Colors.white70),),
-      ),
-      body: Center(
-        child: Column(
-          spacing: 40,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: const Color.fromARGB(255, 88, 72, 72),
+//         title: Text('Home',style: TextStyle(color: Colors.white70),),
+//       ),
+//       body: Center(
+//         child: Column(
+//           spacing: 40,
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
           
-            // Switch(
-            //   value: isConnected,
-            //   activeThumbColor:Colors.deepPurpleAccent,
-            //   activeTrackColor:const Color.fromARGB(255, 255, 68, 71) ,
-            //   inactiveThumbColor: const Color.fromARGB(255, 82, 61, 1),
-            //   inactiveTrackColor: Colors.green,
-            //   onChanged: (value){
-            //   setState(() {
-            //     isConnected=value;
-            //   });
-            //   },
-            // ),
+//             // Switch(
+//             //   value: isConnected,
+//             //   activeThumbColor:Colors.deepPurpleAccent,
+//             //   activeTrackColor:const Color.fromARGB(255, 255, 68, 71) ,
+//             //   inactiveThumbColor: const Color.fromARGB(255, 82, 61, 1),
+//             //   inactiveTrackColor: Colors.green,
+//             //   onChanged: (value){
+//             //   setState(() {
+//             //     isConnected=value;
+//             //   });
+//             //   },
+//             // ),
         
-            // SwitchListTile(
-            //   value: isConnected,
-            //   onChanged: (value) {
-            //     setState(() {
-            //       isConnected=value;
-            //     });
-            //   },   
-            //   title: Text('internet connectivity'),
-            //   subtitle: Text('this is the sub title'),      
-            // ),
+//             // SwitchListTile(
+//             //   value: isConnected,
+//             //   onChanged: (value) {
+//             //     setState(() {
+//             //       isConnected=value;
+//             //     });
+//             //   },   
+//             //   title: Text('internet connectivity'),
+//             //   subtitle: Text('this is the sub title'),      
+//             // ),
         
-            // Checkbox(
-            //   value: isConnected,
-            //   onChanged: (value){
-            //     setState(() {
-            //       isConnected=value!;
-            //     });
-            //   },
-            // ),
-            // CheckboxListTile(
-            //    value: isConnected,
-            //   onChanged: (value){
-            //     setState(() {
-            //       isConnected=value!;
-            //     });
-            //   },
-            //   title: Text('internet connectivity'),
-            //   subtitle: Text('this is the sub title'),   
-            // ),
+//             // Checkbox(
+//             //   value: isConnected,
+//             //   onChanged: (value){
+//             //     setState(() {
+//             //       isConnected=value!;
+//             //     });
+//             //   },
+//             // ),
+//             // CheckboxListTile(
+//             //    value: isConnected,
+//             //   onChanged: (value){
+//             //     setState(() {
+//             //       isConnected=value!;
+//             //     });
+//             //   },
+//             //   title: Text('internet connectivity'),
+//             //   subtitle: Text('this is the sub title'),   
+//             // ),
         
-            // RadioMenuButton(          
-            //   value: 'one',
-            //   groupValue: groupValue,
-            //   onChanged: (value) {
-            //     setState(() {
-            //       groupValue=value;
-            //     });
-            //   },
-            //   child: Text('one'),
-            // ),
+//             // RadioMenuButton(          
+//             //   value: 'one',
+//             //   groupValue: groupValue,
+//             //   onChanged: (value) {
+//             //     setState(() {
+//             //       groupValue=value;
+//             //     });
+//             //   },
+//             //   child: Text('one'),
+//             // ),
         
-            // RadioMenuButton(          
-            //   value: 'two',
-            //   groupValue: groupValue,
-            //   onChanged: (value) {
-            //     setState(() {
-            //       groupValue=value;
-            //     });
-            //   },
-            //   child: Text('two'),
-            // ),
+//             // RadioMenuButton(          
+//             //   value: 'two',
+//             //   groupValue: groupValue,
+//             //   onChanged: (value) {
+//             //     setState(() {
+//             //       groupValue=value;
+//             //     });
+//             //   },
+//             //   child: Text('two'),
+//             // ),
         
-            // RadioMenuButton(          
-            //   value: 'three',
-            //   groupValue: groupValue,
-            //   onChanged: (value) {
-            //     setState(() {
-            //       groupValue=value;
-            //     });
-            //   },
-            //   child: Text('three'),
-            // ),
+//             // RadioMenuButton(          
+//             //   value: 'three',
+//             //   groupValue: groupValue,
+//             //   onChanged: (value) {
+//             //     setState(() {
+//             //       groupValue=value;
+//             //     });
+//             //   },
+//             //   child: Text('three'),
+//             // ),
         
-            // RadioMenuButton(          
-            //   value: 'four',
-            //   groupValue: groupValue,
-            //   onChanged: (value) {
-            //     setState(() {
-            //       groupValue=value;
-            //     });
-            //   },
-            //   child: Text('four'),
-            // ),
-            // RadioListTile(
-            //    value: 'five',
-            //   groupValue: groupValue,
-            //   onChanged: (value) {
-            //     setState(() {
-            //       groupValue=value;
-            //     });
-            //   },
-            //   title: Text('five'),
-            //   subtitle: Text('this is sub title'),
-            // ),
+//             // RadioMenuButton(          
+//             //   value: 'four',
+//             //   groupValue: groupValue,
+//             //   onChanged: (value) {
+//             //     setState(() {
+//             //       groupValue=value;
+//             //     });
+//             //   },
+//             //   child: Text('four'),
+//             // ),
+//             // RadioListTile(
+//             //    value: 'five',
+//             //   groupValue: groupValue,
+//             //   onChanged: (value) {
+//             //     setState(() {
+//             //       groupValue=value;
+//             //     });
+//             //   },
+//             //   title: Text('five'),
+//             //   subtitle: Text('this is sub title'),
+//             // ),
         
         
         
-            // Wrap(
-            //   spacing: 10,
-            //   runSpacing: 10,
-            //   children: [
+//             // Wrap(
+//             //   spacing: 10,
+//             //   runSpacing: 10,
+//             //   children: [
         
-            //     Container(
-            //       color: Colors.amberAccent,
-            //       height: 50,
-            //       width: 50,
-            //     ),
+//             //     Container(
+//             //       color: Colors.amberAccent,
+//             //       height: 50,
+//             //       width: 50,
+//             //     ),
         
-            //      Container(
-            //       color: const Color.fromARGB(255, 64, 255, 109),
-            //       height: 50,
-            //       width: 50,
-            //     ),
+//             //      Container(
+//             //       color: const Color.fromARGB(255, 64, 255, 109),
+//             //       height: 50,
+//             //       width: 50,
+//             //     ),
         
-            //      Container(
-            //       color: const Color.fromARGB(255, 255, 64, 64),
-            //       height: 50,
-            //       width: 50,
-            //     ),
+//             //      Container(
+//             //       color: const Color.fromARGB(255, 255, 64, 64),
+//             //       height: 50,
+//             //       width: 50,
+//             //     ),
         
-            //      Container(
-            //       color: Colors.amberAccent,
-            //       height: 50,
-            //       width: 50,
-            //     ),
+//             //      Container(
+//             //       color: Colors.amberAccent,
+//             //       height: 50,
+//             //       width: 50,
+//             //     ),
         
-            //      Container(
-            //       color: const Color.fromARGB(255, 64, 255, 109),
-            //       height: 50,
-            //       width: 50,
-            //     ),
+//             //      Container(
+//             //       color: const Color.fromARGB(255, 64, 255, 109),
+//             //       height: 50,
+//             //       width: 50,
+//             //     ),
         
-            //      Container(
-            //       color: const Color.fromARGB(255, 255, 64, 64),
-            //       height: 50,
-            //       width: 50,
-            //     ),Container(
-            //       color: Colors.amberAccent,
-            //       height: 50,
-            //       width: 50,
-            //     ),
+//             //      Container(
+//             //       color: const Color.fromARGB(255, 255, 64, 64),
+//             //       height: 50,
+//             //       width: 50,
+//             //     ),Container(
+//             //       color: Colors.amberAccent,
+//             //       height: 50,
+//             //       width: 50,
+//             //     ),
         
-            //      Container(
-            //       color: const Color.fromARGB(255, 64, 255, 109),
-            //       height: 50,
-            //       width: 50,
-            //     ),
+//             //      Container(
+//             //       color: const Color.fromARGB(255, 64, 255, 109),
+//             //       height: 50,
+//             //       width: 50,
+//             //     ),
         
-            //      Container(
-            //       color: const Color.fromARGB(255, 255, 64, 64),
-            //       height: 50,
-            //       width: 50,
-            //     ),Container(
-            //       color: Colors.amberAccent,
-            //       height: 50,
-            //       width: 50,
-            //     ),
+//             //      Container(
+//             //       color: const Color.fromARGB(255, 255, 64, 64),
+//             //       height: 50,
+//             //       width: 50,
+//             //     ),Container(
+//             //       color: Colors.amberAccent,
+//             //       height: 50,
+//             //       width: 50,
+//             //     ),
         
-            //      Container(
-            //       color: const Color.fromARGB(255, 64, 255, 109),
-            //       height: 50,
-            //       width: 50,
-            //     ),
+//             //      Container(
+//             //       color: const Color.fromARGB(255, 64, 255, 109),
+//             //       height: 50,
+//             //       width: 50,
+//             //     ),
         
-            //      Container(
-            //       color: const Color.fromARGB(255, 255, 64, 64),
-            //       height: 50,
-            //       width: 50,
-            //     ),Container(
-            //       color: Colors.amberAccent,
-            //       height: 50,
-            //       width: 50,
-            //     ),
+//             //      Container(
+//             //       color: const Color.fromARGB(255, 255, 64, 64),
+//             //       height: 50,
+//             //       width: 50,
+//             //     ),Container(
+//             //       color: Colors.amberAccent,
+//             //       height: 50,
+//             //       width: 50,
+//             //     ),
         
-            //      Container(
-            //       color: const Color.fromARGB(255, 64, 255, 109),
-            //       height: 50,
-            //       width: 50,
-            //     ),
-            //   ],
-            // )
+//             //      Container(
+//             //       color: const Color.fromARGB(255, 64, 255, 109),
+//             //       height: 50,
+//             //       width: 50,
+//             //     ),
+//             //   ],
+//             // )
 
-            CircularProgressIndicator(
+//             CircularProgressIndicator(
              
-            ),
+//             ),
 
 
         
-            LinearProgressIndicator(
+//             LinearProgressIndicator(
 
-            )
+//             )
 
-          ],
-        ),
-      ),
-    );
-  }
-}
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
