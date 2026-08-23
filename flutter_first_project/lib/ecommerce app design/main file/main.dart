d50fafe9701c255a7c963770ca69ca86e0fa7a02
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-08-24 00:41:56 +0500

    add new file

--- flutter_first_project/lib/ecommerce app design/main file/main.dart
+++ flutter_first_project/lib/ecommerce app design/main file/main.dart
@@ -1,30 +1,30 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_first_project/ecommerce%20app%20design/screens/home_screens.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_first_project/ecommerce%20app%20design/screens/home_screens.dart';
  
- void main (){
-  runApp(MYApp());
- }
+//  void main (){
+//   runApp(MYApp());
+//  }
 
- class MYApp extends StatefulWidget {
-  const MYApp({super.key});
+//  class MYApp extends StatefulWidget {
+//   const MYApp({super.key});
 
-  @override
-  State<MYApp> createState() => _MYAppState();
-}
+//   @override
+//   State<MYApp> createState() => _MYAppState();
+// }
 
-class _MYAppState extends State<MYApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: AppBarTheme(backgroundColor: Colors.white,)
-      ),
+// class _MYAppState extends State<MYApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(
+//         scaffoldBackgroundColor: Colors.white,
+//         appBarTheme: AppBarTheme(backgroundColor: Colors.white,)
+//       ),
 
-      darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.light,
-      debugShowCheckedModeBanner: false,
-      home: HomeScreens(),
-    );
-  }
-}
+//       darkTheme: ThemeData.dark(),
+//       themeMode: ThemeMode.light,
+//       debugShowCheckedModeBanner: false,
+//       home: HomeScreens(),
+//     );
+//   }
+// }
--- flutter_first_project/lib/ecommerce app design/screens/items_baners.dart
+++ flutter_first_project/lib/ecommerce app design/screens/items_baners.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class ItemsBaners extends StatelessWidget {
-  final String image;
-  const ItemsBaners({super.key,required this.image});
+// class ItemsBaners extends StatelessWidget {
+//   final String image;
+//   const ItemsBaners({super.key,required this.image});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-            height: 180,
-            margin:EdgeInsets.symmetric(horizontal: 15) ,
-            decoration:BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage('assets/images/image.jpg'),
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//             height: 180,
+//             margin:EdgeInsets.symmetric(horizontal: 15) ,
+//             decoration:BoxDecoration(
+//               borderRadius: BorderRadius.circular(15),
+//             image: DecorationImage(
+//               fit: BoxFit.cover,
+//               image: AssetImage('assets/images/image.jpg'),
 
-            )
-            ),
-          );
+//             )
+//             ),
+//           );
 
           
-  }
-}
+//   }
+// }
