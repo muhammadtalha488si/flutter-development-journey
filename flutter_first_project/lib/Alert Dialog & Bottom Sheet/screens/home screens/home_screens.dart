d4718f3f1f7e82c7cae56058c98ec6c682e4165f
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-08-27 23:12:11 +0500

    add new file

--- flutter_first_project/lib/Alert Dialog & Bottom Sheet/screens/home screens/home_screens.dart
+++ flutter_first_project/lib/Alert Dialog & Bottom Sheet/screens/home screens/home_screens.dart
@@ -6,7 +6,7 @@ class HomeScreens extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.pink,
+        backgroundColor: const Color.fromARGB(255, 248, 165, 193),
         title: Text('Home'),
       ),
     );
