9b072e6bd925fad21639da1bfd112db090e050d8
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-08-28 00:48:16 +0500

    add new file

--- flutter_first_project/lib/Alert Dialog & Bottom Sheet/main file/mai.dart
+++ flutter_first_project/lib/Alert Dialog & Bottom Sheet/main file/mai.dart
@@ -1,18 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_first_project/Alert%20Dialog%20&%20Bottom%20Sheet/screens/home%20screens/home_screens.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_first_project/Alert%20Dialog%20&%20Bottom%20Sheet/screens/home%20screens/home_screens.dart';
 
-void main(){
-  runApp(MyApp());
-}
+// void main(){
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreens(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: HomeScreens(),
+//     );
+//   }
+// }
--- flutter_first_project/lib/Alert Dialog & Bottom Sheet/screens/home screens/home_screens.dart
+++ flutter_first_project/lib/Alert Dialog & Bottom Sheet/screens/home screens/home_screens.dart
@@ -1,72 +1,72 @@
-import 'package:flutter/material.dart';
-class HomeScreens extends StatelessWidget {
-  const HomeScreens({super.key});
+// import 'package:flutter/material.dart';
+// class HomeScreens extends StatelessWidget {
+//   const HomeScreens({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 248, 165, 193),
-        title: Text('Home'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          spacing: 15,
-          children: [
-          ElevatedButton(
-          onPressed: () {
-           showDialog(
-            context: context,
-            builder:(context){
-              return AlertDialog(
-                title: Text('Logout'),
-                icon: Icon(Icons.logout),
-                content: Text('Are you sure you want logout ?'),
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: const Color.fromARGB(255, 248, 165, 193),
+//         title: Text('Home'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           spacing: 15,
+//           children: [
+//           ElevatedButton(
+//           onPressed: () {
+//            showDialog(
+//             context: context,
+//             builder:(context){
+//               return AlertDialog(
+//                 title: Text('Logout'),
+//                 icon: Icon(Icons.logout),
+//                 content: Text('Are you sure you want logout ?'),
 
-                actions: [
-                  ElevatedButton(
-                    onPressed:(){
-                   Navigator.pop(context);
-                    } ,
-                  child:Text('No')
-                  ),
-                  ElevatedButton(
-                    onPressed:(){
-                   Navigator.pop(context);
-                    } ,
-                  child:Text('yes ')
-                  ),
-                ],
-              );
-            }
-            );
-          },
-          child: Text('show Dialog'),
-          ),  
+//                 actions: [
+//                   ElevatedButton(
+//                     onPressed:(){
+//                    Navigator.pop(context);
+//                     } ,
+//                   child:Text('No')
+//                   ),
+//                   ElevatedButton(
+//                     onPressed:(){
+//                    Navigator.pop(context);
+//                     } ,
+//                   child:Text('yes ')
+//                   ),
+//                 ],
+//               );
+//             }
+//             );
+//           },
+//           child: Text('show Dialog'),
+//           ),  
            
-          ElevatedButton(
-          onPressed: () {
-            showModalBottomSheet(
-            context: context ,
-            showDragHandle: true,
-            builder: (context){
-              return ListView(
-              children: [
-              ListTile(
-             title: Text('this is title'),
-              ),
-                ],
-              );
-            },
-            );
-          },
-          child: Text('show Bottom Sheet'),
-          ),
+//           ElevatedButton(
+//           onPressed: () {
+//             showModalBottomSheet(
+//             context: context ,
+//             showDragHandle: true,
+//             builder: (context){
+//               return ListView(
+//               children: [
+//               ListTile(
+//              title: Text('this is title'),
+//               ),
+//                 ],
+//               );
+//             },
+//             );
+//           },
+//           child: Text('show Bottom Sheet'),
+//           ),
         
-          ],
-        ),
-      ),
-    );
-  }
-}
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
