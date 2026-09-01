40982def0fbd7417dd26bdcf7aa66c04445e97dd
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-09-02 01:43:24 +0500

    add new file

--- flutter_first_project/lib/stack & positioned_creating over lapping widget/screens/home_screens.dart
+++ flutter_first_project/lib/stack & positioned_creating over lapping widget/screens/home_screens.dart
@@ -1,63 +1,63 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class HomeScreens extends StatelessWidget {
-//   const HomeScreens({super.key});
+class HomeScreens extends StatelessWidget {
+  const HomeScreens({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.amberAccent,
-//         title: Text('HOME PAGE'),
-//       ),
-//       body: Stack(
-//         children: [
-//           Positioned(
-//             left: 0,
-//             bottom: 0,
-//             child: Container(
-//               color: Colors.blue,
-//               height: 200,
-//               width: 200,
-//             ),
-//           ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amberAccent,
+        title: Text('HOME PAGE'),
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            bottom: 0,
+            child: Container(
+              color: Colors.blue,
+              height: 200,
+              width: 200,
+            ),
+          ),
 
-//           Positioned(
-//             left: 0,
-//             bottom: 0,
-//             child: Container(
-//               color: const Color.fromARGB(255, 243, 33, 219),
-//               height: 100,
-//               width: 100,
-//             ),
-//           ),
-//           Positioned(
-//           top: -50,
-//           right: -50,
-//           child: Container(
-//             height: 200,
-//             width: 200,
-//             decoration: BoxDecoration(
-//               shape: BoxShape.circle,
-//               color: Colors.deepOrange,
-//             ),
-//           ),
-//           ),
+          Positioned(
+            left: 0,
+            bottom: 0,
+            child: Container(
+              color: const Color.fromARGB(255, 243, 33, 219),
+              height: 100,
+              width: 100,
+            ),
+          ),
+          Positioned(
+          top: -50,
+          right: -50,
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.deepOrange,
+            ),
+          ),
+          ),
 
-// Positioned(
-//           top: -20,
-//           right: -20,
-//           child: Container(
-//             height: 100,
-//             width: 100,
-//             decoration: BoxDecoration(
-//               shape: BoxShape.circle,
-//               color: const Color.fromARGB(255, 74, 255, 34),
-//             ),
-//           ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+Positioned(
+          top: -20,
+          right: -20,
+          child: Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: const Color.fromARGB(255, 74, 255, 34),
+            ),
+          ),
+          ),
+        ],
+      ),
+    );
+  }
+}
