cd44b06c2faeabae683526c8c67406dc98a14a22
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-09-02 01:34:15 +0500

    add new file

--- flutter_first_project/lib/popup Menu & Dropdown/main file/main.dart
+++ flutter_first_project/lib/popup Menu & Dropdown/main file/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_first_project/popup%20Menu%20&%20Dropdown/screens/home_screen.dart';
 
 void main(){
   runApp(MyApp());
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
   }
 }
--- flutter_first_project/lib/popup Menu & Dropdown/screens/home_screen.dart
+++ flutter_first_project/lib/popup Menu & Dropdown/screens/home_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+int selected=1;
+
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +17,44 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.lightGreenAccent,
         title: Text('Home'),
+        actions: [
+          PopupMenuButton<int>(
+            onSelected: (value) {
+              print('Value is $value');
+            },
+            itemBuilder:
+            (context){
+              return [
+                 for(int i=1;i<=5;i++)
+                PopupMenuItem<int>(
+                  value: 1,
+                  child:Text('item $i'),
+                   ),
+                   
+              ];
+            } )
+        ],
       ),
+      
+    body: Center(
+      child: DropdownButton(
+        value: selected,
+        onChanged: (value) {
+          setState(() {
+            
+          });
+        },
+
+        items: [
+          for(int i=1;i<=10;i++)
+          DropdownMenuItem(
+            value: i,
+            child: Text('value is $i'),
+          )
+        ],
+      ),
+    ),
+
     );
   }
 }
