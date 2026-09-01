bee448c9c996a18f85de1860a89210e5538a1f70
muhammadtalha488si <muhammadtalhadurrani044@gmail.com>
2026-09-02 01:42:41 +0500

    add new file

--- flutter_first_project/lib/responsive_ui/screens/home_screens.dart
+++ flutter_first_project/lib/responsive_ui/screens/home_screens.dart
@@ -1,46 +1,46 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class HomeScreens extends StatelessWidget {
-//   const HomeScreens({super.key});
+class HomeScreens extends StatelessWidget {
+  const HomeScreens({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.brown,
-//         title: Text('HOME'),
-//       ),
-//       body: Flex(
-//         spacing: 10,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.brown,
+        title: Text('HOME'),
+      ),
+      body: Flex(
+        spacing: 10,
         
-//         direction: Axis.horizontal,
-//         children: [
-//           Expanded(
-//             child: Container(
-//               color: Colors.amber,
-//               width: double.infinity,
-//             ),
-//           ),
-//            Expanded(
-//             child: Container(
-//               color: const Color.fromARGB(255, 4, 1, 36),
-//               width: double.infinity,
-//             ),
-//           ),
-//            Expanded(
-//             child: Container(
-//               color: const Color.fromARGB(255, 49, 18, 128),
-//               width: double.infinity,
-//             ),
-//           ),
-//            Expanded(
-//             child: Container(
-//               color: const Color.fromARGB(255, 76, 209, 5),
-//               width: double.infinity,
-//             ),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
+        direction: Axis.horizontal,
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.amber,
+              width: double.infinity,
+            ),
+          ),
+           Expanded(
+            child: Container(
+              color: const Color.fromARGB(255, 4, 1, 36),
+              width: double.infinity,
+            ),
+          ),
+           Expanded(
+            child: Container(
+              color: const Color.fromARGB(255, 49, 18, 128),
+              width: double.infinity,
+            ),
+          ),
+           Expanded(
+            child: Container(
+              color: const Color.fromARGB(255, 76, 209, 5),
+              width: double.infinity,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
